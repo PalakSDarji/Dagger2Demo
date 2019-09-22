e4bbdc20dd55c7e765148d7128ba6d1241608a74
palaksdarji <palaksdarji@gmail.com>
2019-09-22 08:29:50 +0530

    coroutines added

---
+++ app/src/main/java/com/feedr/blog/dagger2demo/coroutines/CoroutineDemoActivity.kt
@@ -0,0 +1,14 @@
+package com.feedr.blog.dagger2demo.coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.feedr.blog.dagger2demo.R
+
+class CoroutineDemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coroutine_demo)
+
+    }
+}
---
+++ app/src/main/java/com/feedr/blog/dagger2demo/notification/NotifActivity.kt
@@ -0,0 +1,13 @@
+package com.feedr.blog.dagger2demo.notification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.feedr.blog.dagger2demo.R
+
+class NotifActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notif)
+    }
+}

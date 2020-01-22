c517fa5ac8e865d01354223b9022e65201ca6223
Palak.darji <palak.darji.hs@gmail.com>
2020-01-22 18:49:58 +0530

    animation translation Y added on start

--- app/src/main/java/com/feedr/blog/dagger2demo/coroutines/CoroutineDemoActivity.kt
+++ app/src/main/java/com/feedr/blog/dagger2demo/coroutines/CoroutineDemoActivity.kt
@@ -1,8 +1,10 @@
 package com.feedr.blog.dagger2demo.coroutines
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.feedr.blog.dagger2demo.R
+import kotlinx.android.synthetic.main.activity_coroutine_demo.*
 
 class CoroutineDemoActivity : AppCompatActivity() {
 
@@ -10,5 +12,16 @@ class CoroutineDemoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutine_demo)
 
+        ll.post {
+            ll.translationY = - ll.height.toFloat();
+            ObjectAnimator.ofFloat(ll,"translationY",-ll.height.toFloat(),0f).apply {
+                duration = 1000
+                start()
+            }
+        }
+
+
+
     }
+
 }
